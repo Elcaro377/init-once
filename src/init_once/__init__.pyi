9444fd688e63276c992aaef8491d90a1d6a6b420
Priestess. <729388144@qq.com>
2026-04-06 16:19:43 +0800

    add an underscore prefix to internal files, supports up to 3.14

--- src/init_once/__init__.pyi
+++ src/init_once/__init__.pyi
@@ -1,10 +1,10 @@
 import sys
 
 if sys.version_info >= (3, 12):
-    from .init_once_3_12 import init_once
+    from ._init_once_3_12 import init_once
 
 else:
-    from.init_once_3_8 import init_once
+    from._init_once_3_8 import init_once
 
 
 __all__ = ["init_once"]
